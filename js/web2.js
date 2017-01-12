15885907154c911d30536b5a8a03802cc4032680
sindhujaadevi <pencil4sind@gmail.com>
2017-01-12 18:47:06 +0530

    sem1

---
+++ js/web2.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $('.ui.vertical.menu').hide();
+  $('.right.menu.open').on("click", function(e) {
+    e.preventDefault();
+    $('.ui.vertical.menu').toggle();
+  });
+  $('.small.modal')
+    .modal('show');
+
+  $('.ui.dropdown').dropdown();
+  $('.ui.card .image').dimmer({
+    on: 'hover'
+  });
+  $('.ui.rating').rating({
+    maxRating: 5
+  });
+
+
+
+  $('.button.view').on('click', (function() {
+    $('#album').fadeOut("slow", function() {
+
+    });
+  }));
+
+  $('.button.back').on('click', (function() {
+    $('#album_items').fadeOut("slow", function() {
+      $('#album').fadeIn("slow");
+    });
+  }));
+});
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
